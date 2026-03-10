bd37d6c981d77d8ba923b98bd29ec57e95817450
dgashu7838 <dgashu.com@gmail.com>
2026-03-10 20:53:43 +0530

    top navbar

--- header.php
+++ header.php
@@ -15,15 +15,32 @@
         <div class="topbar d-flex align-items-center dark-background">
       <div class="container d-flex justify-content-center justify-content-md-between">
         <div class="contact-info d-flex align-items-center">
-          <i class="bi bi-envelope d-flex align-items-center"><a
-              href="mailto:contact@example.com">contact@example.com</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+          <?php if(get_theme_mod("topbar_email")){?>
+            <i class="bi bi-envelope d-flex align-items-center"><a
+              href="mailto:<?php echo esc_attr(get_theme_mod("topbar_email")); ?>"><?php echo esc_html(get_theme_mod("topbar_email")); ?></a></i>
+          <?php } ?>
+
+          <?php if(get_theme_mod("topbar_number")){?>
+            <i class="bi bi-phone d-flex align-items-center ms-4"><a
+              href="tel:<?php echo esc_attr(get_theme_mod("topbar_number")); ?>"><?php echo esc_html(get_theme_mod("topbar_number")); ?></a></i>
+          <?php } ?>
         </div>
         <div class="social-links d-none d-md-flex align-items-center">
-          <a href="#!" class="twitter"><i class="bi bi-twitter-x"></i></a>
-          <a href="#!" class="facebook"><i class="bi bi-facebook"></i></a>
-          <a href="#!" class="instagram"><i class="bi bi-instagram"></i></a>
-          <a href="#!" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          <?php 
+              $socials = [
+                  "twitter"=>"bi bi-twitter-x",
+                  "facebook"=>"bi bi-facebook",
+                  "instagram"=>"bi bi-instagram",
+                  "linkedin"=>"bi bi-linkedin",
+              ];
+
+              foreach($socials as $social=>$icon){
+                $url = get_theme_mod("topbar_{$social}");
+                if($url){ ?>
+                  <a href="<?php echo esc_url($url); ?>" class="<?php echo esc_attr($social) ?>"><i class="<?php echo esc_attr($icon) ?>"></i></a>
+              <?php   }
+              }
+          ?>
         </div>
       </div>
     </div><!-- End Top Bar -->
--- inc/topNav.php
+++ inc/topNav.php
@@ -22,6 +22,52 @@ function myThemeToNavCustomizer($wp_customize){
         "type"=>"checkbox",
     ]);
 
+    // email
+
+    $wp_customize->add_setting("topbar_email",[
+        "default"=>"contact@example.com",
+        "sanitize_callback"=>"sanitize_email",
+    ]);
+
+    $wp_customize->add_control("topbar_email",[
+        "label"=>__("Email Address","myTheme"),
+        "section"=>"clinic_topbar_section",
+        "type"=>"text",
+    ]);
+
+    // phone number
+    $wp_customize->add_setting("topbar_number",[
+        "default"=>"+1 5589 55488 55",
+        "sanitize_callback"=>"sanitize_text_field",
+    ]);
+
+    $wp_customize->add_control("topbar_number",[
+        "label"=>__("Phone Number","myTheme"),
+        "section"=>"clinic_topbar_section",
+        "type"=>"text",
+    ]);
+
+    // social
+
+    $socials = [
+        "twitter",
+        "facebook",
+        "instagram",
+        "linkedin",
+    ];
+
+    foreach($socials as $social) {
+        $wp_customize->add_setting("topbar_{$social}",[
+            "default"=>"#!",
+            "sanitize_callback"=>"esc_url_raw",
+        ]);
+
+        $wp_customize->add_control("topbar_{$social}",[
+            "label"=>__(ucfirst($social).' URL',"myTheme"),
+            "section"=>"clinic_topbar_section",
+            "type"=>"url",
+        ]);
+    }
 }
 
 add_action("customize_register","myThemeToNavCustomizer");
